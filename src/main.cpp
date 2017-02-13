64c7de906bd1e6db73b3e11cb615edca486baa04
natis1 <eli.stonium@gmail.com>
2017-02-13 16:11:59 -0700

    Add message to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "objectcolorpicker.h"
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
+    std::cout << "It compiled successfully. We are good to go" << std::endl;
     QApplication app(argc, argv);
     objectColorPicker w;
     w.show();
